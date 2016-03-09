f1208299b3556211b440ad056a342cf5d48e05fa
YT <yt_79@126.com>
2016-03-09 13:46:48 +0800

    change gun cc collections.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=myc
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/myc
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/myc
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=myc.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/myc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/myc.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=myc
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/myc
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/myc
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=myc.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/myc.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/myc.tar
 #
 # include compiler specific variables
 #
